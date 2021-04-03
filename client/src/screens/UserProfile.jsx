f0247d87bb13afc75c4443ccb40da3bd6ad2c1dc
Jason Quizhpi <jasonquizhpi@Jasons-MacBook-Pro-2.local>
2021-04-02 22:36:40 -0400

    added slice to userprofile

--- client/src/screens/UserProfile.jsx
+++ client/src/screens/UserProfile.jsx
@@ -34,7 +34,7 @@ function UserProfile(props) {
 
   return (
     <div>
-      <h3> Hello {currentUser.name.slice(0,2)} .... I mean User!</h3>
+      <h3> Hello {currentUser.name.slice(0, 2)} .... I mean User {currentUser.id}!</h3>
       {currentUser &&
         posts.filter((post) => {
           
