475a28f64281e944f84779fe081c5800bbceca04
Jason Quizhpi <jasonquizhpi@Jasons-MacBook-Pro-2.local>
2021-04-06 14:13:19 -0400

    css fixed

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import './Modal.css'
+import { motion } from "framer-motion"
 
 function Modal(props) {
   const {  setShow} = props;
+  const variants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+  }
   return (
+    <motion.div
+    initial="hidden"
+   animate="visible"
+   variants={variants}
+   >
     <div className="modal" onClick={(e) => setShow(false)}>
       <div className="modal-children" onClick={(e) => e.stopPropagation()}>
       {props.children}
@@ -12,6 +22,7 @@ function Modal(props) {
        </div>
       
     </div>
+    </motion.div>
   );
 }
 
--- client/src/screens/Posts/Posts.jsx
+++ client/src/screens/Posts/Posts.jsx
@@ -3,13 +3,12 @@ import { Link } from "react-router-dom";
 import Likes from "../../components/Likes";
 import Modal from "../../components/Modal";
 import CreatePost from "../CreatePost/CreatePost";
-import "./Posts.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faComments } from "@fortawesome/free-solid-svg-icons";
 import { faEdit } from "@fortawesome/free-solid-svg-icons";
 import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
-import { motion } from "framer-motion"
-
+import { motion } from "framer-motion";
+import "./Posts.css";
 
 function Posts(props) {
   const { posts, setPosts, handleDelete, currentUser } = props;
@@ -29,128 +28,101 @@ function Posts(props) {
     setShow(!show);
   };
 
-  const pageVariants = {
-    initial: {
-      opacity: 0,
-      x: "-100vw",
-      scale: 0.8
-    },
-    in: {
-      opacity: 1,
-      x: 0,
-      scale: 1
-    },
-    out: {
-      opacity: 0,
-      x: "100vw",
-      scale: 1.2
-    }
-  };
-  
-  const pageTransition = {
-    type: "tween",
-    ease: "anticipate",
-    duration: 0.9
-  };
-  
- 
+  const variants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+  }
+
 
   return (
     <motion.div
-    initial="initial"
-    animate="in"
-    exit="out"
-    variants={pageVariants}
-    transition={pageTransition}
+     initial="hidden"
+    animate="visible"
+    variants={variants}
     >
-    <div className="body">
-      <CreatePost
-        className="createPost"
-        currentUser={currentUser}
-        posts={posts}
-        setPosts={setPosts}
-      />
+      <div className="body">
+        <CreatePost
+          className="createPost"
+          currentUser={currentUser}
+          posts={posts}
+          setPosts={setPosts}
+        />
+        <br />
+        <div >
+          {posts
+            .slice(0)
+            .reverse()
+            .map((post) => (
+              <div key={post.id}>
+                <div className="lol">
+                  <div className="post">{post.content}</div>
 
-      <br />
-
-      <div className="index">
-        {posts
-          .slice(0)
-          .reverse()
-          .map((post) => (
-            <div key={post.id}>
-              <div className="lol">
-                <div className="post">{post.content}</div>
-
-                {currentUser?.id === post.user_id && (
-                  <div className="userContainer" key={post.id}>
-                    <div className="small">
+                  {currentUser?.id === post.user_id && (
+                    <div className="userContainer" key={post.id}>
+                      <div className="small">
+                        <Likes
+                          allLikes={post.likes}
+                          posts={posts}
+                          postId={post.id}
+                        />
+                        <Link to={`/posts/${post.id}`}>
+                          {icon} {post.comments.length}
+                        </Link>
+                        <Link to={`/posts/${post.id}/edit`}>
+                          <button className="edit">{editIcon}</button>
+                        </Link>
+                        <button
+                          className="delete"
+                          onClick={() => {
+                            setShow((curr) => !curr);
+                            setPostId(post.id);
+                          }}
+                        >
+                          {trashIcon}
+                        </button>
+                      </div>
+                    </div>
+                  )}
+                  {currentUser?.id !== post.user_id && (
+                    <div className="userContainer">
                       <Likes
                         allLikes={post.likes}
                         posts={posts}
                         postId={post.id}
                       />
-                      <Link to={`/posts/${post.id}`}>
+                      <Link className="comments" to={`/posts/${post.id}`}>
                         {icon} {post.comments.length}
                       </Link>
-                      <Link to={`/posts/${post.id}/edit`}>
-                        <button className="edit">{editIcon}</button>
-                      </Link>
-                      <button
-                        className="delete"
-                        onClick={() => {
-                          setShow((curr) => !curr);
-                          setPostId(post.id);
-                        }}
-                      >
-                        {trashIcon}
-                      </button>
                     </div>
-                  </div>
-                )}
-                {currentUser?.id !== post.user_id && (
-                  <div className="userContainer">
-                    <Likes
-                      allLikes={post.likes}
-                      posts={posts}
-                      postId={post.id}
-                    />
-                    <Link className="comments" to={`/posts/${post.id}`}>
-                      {icon} {post.comments.length}
-                    </Link>
-                  </div>
-                )}
+                  )}
+                </div>
               </div>
-            </div>
-          ))}
-      </div>
-      {show && (
-        <Modal
-          posts={posts}
-          handleDelate={handleDelete}
-          show={show}
-          postsId={posts.id}
-          setShow={setShow}
-          toggleModal={toggleModal}
-        >
-          <p>Are you sure?</p>
-          <br />
-          <button className="no" onClick={() => setShow(false)}>
-            no
-          </button>
-          <button
-            className="yes"
-            onClick={() => {
-              handleDelete(postId);
-              setShow(false);
-            }}
+            ))}
+        </div>
+        {show && (
+          <Modal
+            posts={posts}
+            handleDelate={handleDelete}
+            show={show}
+            postsId={posts.id}
+            setShow={setShow}
+            toggleModal={toggleModal}
           >
-            yes
-          </button>
-        </Modal>
-      )}
+            <p>Are you sure?</p>
+            <br />
+            <button className="no" onClick={() => setShow(false)}>
+              no </button>
+            <button
+              className="yes"
+              onClick={() => {
+                handleDelete(postId);
+                setShow(false);
+              }}
+            > yes </button>
+          </Modal>
+        )}
       </div>
-      </motion.div>
+     </motion.div>
   );
 }
 
